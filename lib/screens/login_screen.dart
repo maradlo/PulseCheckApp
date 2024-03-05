a5cc491cd880102cc0169d32105d48d1fdc2a253
Marek Zacik <zacikm@icloud.com>
2024-03-05 13:30:51 +0100

    Initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,127 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:pulse_check/screens/home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void handleLogin() async {
+    if (usernameController.text == 'admin@pulsecheck.com' &&
+        passwordController.text == 'admin') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const HomeScreen(), fullscreenDialog: true),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.blueGrey.shade600),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const FlutterLogo(size: 100),
+                const Text(
+                  'Pulse Check',
+                  style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+                const SizedBox(height: 2.0),
+                const Text(
+                  'Dochádzkový systém',
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w600,
+                      color: Color.fromRGBO(255, 255, 255, 0.6)),
+                ),
+                const SizedBox(height: 80.0),
+                TextField(
+                  controller: usernameController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        const EdgeInsets.fromLTRB(20.0, 12.0, 20.0, 12.0),
+                    labelText: 'E-mail',
+                    fillColor: Colors.white,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        const EdgeInsets.fromLTRB(20.0, 12.0, 20.0, 12.0),
+                    labelText: 'Heslo',
+                    fillColor: Colors.white,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(255, 255, 255, 0.2),
+                      foregroundColor: const Color.fromRGBO(255, 255, 255, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    ),
+                    child: const Text('Prihlásiť sa',
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.w600)),
+                    onPressed: () {
+                      handleLogin();
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                const Text(
+                    "Ak nepoznáte prihlasovacie údaje, kotaktujte IT oddelenie na tel. čísle: 0917 722 223",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
